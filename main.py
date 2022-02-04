6598e7c475ecf870451a7efc695de97c9e7af119
SileboxUnderfined <valerkafm@gmail.com>
2022-02-05 00:15:29 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import vk_api
+from flask import Flask, request
+from vk_api.utils import get_random_id
+from os import environ
+
+app = Flask(__name__)
+
+@app.route('/catBot', methods=['POST'])
+def bot():
+    pass
+
+
+if __name__ in "__main__":
+    app.run(host='0.0.0.0',port=environ['PORT'],debug=False)
