1b7db18d2587ac398b802ffe8e8f31569c714b86
SileboxUnderfined <valerkafm@gmail.com>
2022-02-05 11:01:26 +0300

    editing method uploading photos

--- cat.py
+++ cat.py
@@ -1,17 +1,11 @@
 import requests
-from vk_api import VkUpload
-from os import environ
 
 class Cat:
     @staticmethod
-    def getRandomCat(vk):
-        upload = VkUpload(vk)
+    def getRandomCat():
         imgarr = requests.get("https://api.thecatapi.com/v1/images/search")
         img = imgarr.json()[0]['url']
-        photo = upload.photo(photos=img,album_id=environ['ALBUM_ID'],group_id=environ['GROUP_ID'])
-        photoProps = photo[0]
-        attachment = f'photo{photoProps["owner_id"]}_{photoProps["id"]}_{photoProps["access_key"]}'
-        return attachment
+        return img
 
 """if __name__ in "__main__":
     a = Cat()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from vk_api.utils import get_random_id
 from vk_api import VkApi
+from vk_api import VkUpload
 from os import environ
 from keyboards import startKeyboard
 from cat import Cat
@@ -30,12 +31,17 @@ def bot():
                 bs.messages.send(message="Используй клавиатуру!",random_id=get_random_id(),user_id=message['from_id'],keyboard=startKeyboard())
 
             elif message['text'] == "Хочу кота":
-                bs.messages.send(message="Лови кота!",random_id=get_random_id(),user_id=message['from_id'],keyboard=startKeyboard(),attachment=cat.getRandomCat(bs))
+                img = cat.getRandomCat()
+                photo = upload.photo_messages(img,message['from_id'])
+                photoProps = photo[0]
+                attachment = f'photo_{photoProps["owner_id"]}_{photoProps["photo_id"]}_{photoProps["access_key"]}'
+                bs.messages.send(message="Лови кота!",random_id=get_random_id(),user_id=message['from_id'],keyboard=startKeyboard(),attachment=attachment)
 
     return 'ok'
 
 if __name__ in "__main__":
     BotSession = VkApi(token=environ['VK_API_KEY'])
     bs = BotSession.get_api()
+    upload = VkUpload(bs)
     cat = Cat()
     app.run(host='0.0.0.0',port=environ['PORT'],debug=False)
