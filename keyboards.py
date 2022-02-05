059133b13ae15aa0523493c00517f25caf46a021
SileboxUnderfined <valerkafm@gmail.com>
2022-02-05 10:42:36 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -2,5 +2,4 @@ from vk_api.keyboard import *
 def startKeyboard():
     keyboard = VkKeyboard()
     keyboard.add_button("Хочу кота",color=VkKeyboardColor.POSITIVE)
-    keyboard.add_line()
     return keyboard.get_keyboard()
